5c320b085b11c4d815b547388b030f8cb01641dd
NUrhussen MuhammedTuem <161613865+nurhusenm@users.noreply.github.com>
2024-04-19 00:28:10 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const colors = ["green", "red", 
+"rgba(133,122,200)", "#f15025"];
+
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+
+    const randomNumber = getRandomNumber();
+
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.innerHTML = colors[randomNumber]
+})
+ function getRandomNumber () {
+    return Math.floor(Math.random() * colors.length)
+}
+
+
+
+
+/*const color = document.querySelector('.color')
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", function(){
+    const randomNumber = getRandomNumber();
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent =  colors[randomNumber]
+})
+
+function getRandomNumber(){
+
+    return Math.floor(Math.random() * colors.length);
+}*/
